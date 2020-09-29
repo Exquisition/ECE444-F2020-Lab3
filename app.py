a5f054bd075e92f5ae1342c7d5cf84a5dfab6941
Exquisition <l.zhou@mail.utoronto.ca>
2020-09-28 21:35:09 -0400

    completed activity 1

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
@@ -16,4 +19,4 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
